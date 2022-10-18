1812d9a1384e5356667434e132f040c5795d52f8
dangertater <106702944+dangertater@users.noreply.github.com>
2022-10-18 10:01:29 -0600

    just starting

---
+++ callbacks.js
@@ -0,0 +1,28 @@
+let posts = [
+	{ title: "Post one", body: "This is post one" },
+	{ title: "Post two", body: "This is post two" },
+]
+
+// let getPosts = () => {
+// 	setTimeout(() => {
+// 		let output = ""
+// 		posts.forEach((post, index) => {
+// 			output += `<li>${post.title}</li>`
+// 		})
+// 		document.body.innerHTML = output
+// 	}, 1000)
+// }
+
+// let createPost = (post, callback) => {
+//     return new Promise((resolve,reject) => {
+//         setTimeout(())
+//     })
+// 	setTimeout(() => {
+// 		posts.push(post)
+// 		callback()
+// 	}, 2000)
+// }
+
+// createPost({ title: "Post Three", body: "this is post three" }, getPosts)
+// 	.then(getPosts)
+// 	.catch((err) => console.log(err))
