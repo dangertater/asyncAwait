903acc45615dc8ed0164b76d403676aac37bf889
dangertater <106702944+dangertater@users.noreply.github.com>
2022-10-18 10:48:44 -0600

    displays issue of server retrieval time being too long)

--- callbacks.js
+++ callbacks.js
@@ -2,16 +2,30 @@ let posts = [
 	{ title: "Post one", body: "This is post one" },
 	{ title: "Post two", body: "This is post two" },
 ]
+let doc1 = document.getElementById("main")
+//cycles through posts array, each indy post will be added to the output string,
+//----output string added to the html, all after 1000ms
+let getPosts = () => {
+	setTimeout(() => {
+		let output = ""
+		posts.forEach((post, index) => {
+			output += `<li>${post.title}</li>`
+		})
+		doc1.innerHTML = output
+	}, 1000)
+}
 
-// let getPosts = () => {
-// 	setTimeout(() => {
-// 		let output = ""
-// 		posts.forEach((post, index) => {
-// 			output += `<li>${post.title}</li>`
-// 		})
-// 		document.body.innerHTML = output
-// 	}, 1000)
-// }
+let createPost = (post) => {
+	setTimeout(() => {
+		posts.push(post)
+		//depending on the milisecond number on below line, the createPost function
+		//----is being executed after getPosts function despite createPost()
+		//----being called before getPosts()
+	}, 2000)
+}
+
+createPost({ title: "Post three", body: "This is post three" })
+getPosts()
 
 // let createPost = (post, callback) => {
 //     return new Promise((resolve,reject) => {
@@ -26,3 +40,9 @@ let posts = [
 // createPost({ title: "Post Three", body: "this is post three" }, getPosts)
 // 	.then(getPosts)
 // 	.catch((err) => console.log(err))
+
+// if (typeof window !== "undefined") {
+// 	console.log("you are on the browser")
+// } else {
+// 	console.log("you are on the server")
+// }
